25c3ae3ce257ddce2afda8a4c33002d217f32186
Mr-Philo <1347549342@qq.com>
2024-05-28 11:43:52 +0000

    add nll loss function and unit test

--- msamp/operators/loss_fn/loss_fn.py
+++ msamp/operators/loss_fn/loss_fn.py
@@ -35,9 +35,41 @@ class _ScalingSum(torch.autograd.Function):
         grad.scaling_meta = grad_scaling_tensor.meta
         grad.is_fp8_form = True
         
-        assert grad.shape == ctx.shape, f"Activation grad shape should be the same as input shape {ctx.shape}, but got {grad.shape}"
+        assert grad.shape == ctx.shape, f"Loss grad shape should be the same as input shape {ctx.shape}, but got {grad.shape}"
         return grad, None
 
+
+class _ScalingNLLLoss(torch.autograd.Function):  
+    '''NLLLoss function for FP8 input and output.'''  
+      
+    @staticmethod  
+    def forward(ctx, inp, target):  
+        ctx.shape = inp.shape  
+        ctx.true_out_shape = inp.shape[:-1] + (inp.shape[-1]*2, )       # same as inp.view(dtype=torch.uint8).shape
+        inp = TypeCast.cast_from_fp8(inp.view(dtype=torch.uint8), inp.scaling_meta,  Dtypes.kfloat16)  
+        ctx.save_for_backward(inp, target)  
+        loss = torch.nn.functional.nll_loss(inp, target)  
+        return loss  
+      
+    @staticmethod  
+    def backward(ctx, grad_output):  
+        inp, target = ctx.saved_tensors  
+          
+        # Compute the gradient of the loss w.r.t the input  
+        N = inp.size(0)
+        grad_input = torch.zeros(ctx.true_out_shape, dtype=torch.float16).cuda()
+        # Calculate the gradient of the loss with respect to input
+        grad_input.scatter_(1, target.unsqueeze(1), -1.0 / N)
+        # Scale by the incoming gradient (chain rule)
+        grad_input *= grad_output  
+          
+        grad_scaling_tensor = grad_input.cast(Dtypes.kfloat8_e5m2, meta=ScalingMeta(Dtypes.kfloat8_e5m2))  
+        grad = grad_scaling_tensor.value.view(dtype=torch.float16)  
+        grad.scaling_meta = grad_scaling_tensor.meta  
+        grad.is_fp8_form = True  
+          
+        assert grad.shape == ctx.shape, f"Loss grad shape should be the same as input shape {ctx.shape}, but got {grad.shape}"  
+        return grad, None
 
 class Loss_fn:
     """Loss function class to FP8 precision"""
@@ -53,7 +85,24 @@ class Loss_fn:
             torch.Tensor: Output tensor.
         """
         if inp.is_fp8_form:
-            assert inp.dtype == torch.float16, f"Currently _ScalingSum function only supports float16 input tensor when it is in fp8 form, but got {input.dtype}"
+            assert inp.dtype == torch.float16, f"Currently _ScalingSum function only supports float16 input tensor when it is in fp8 form, but got {inp.dtype}"
             return _ScalingSum.apply(inp, dtype)
         else:
             return torch.sum(inp, dtype)
+        
+    @classmethod
+    def nll_loss(cls, inp: torch.Tensor, target: torch.Tensor) -> torch.Tensor:  
+        """NLLLoss function for FP8 input and output.  
+          
+        Args:  
+            inp (torch.Tensor): Input tensor.  
+            target (torch.Tensor): Target tensor.  
+          
+        Returns:  
+            torch.Tensor: Output tensor.  
+        """  
+        if inp.is_fp8_form:  
+            assert inp.dtype == torch.float16, f"Currently _ScalingNLLLoss function only supports float16 input tensor when it is in fp8 form, but got {inp.dtype}"  
+            return _ScalingNLLLoss.apply(inp, target)  
+        else:  
+            return torch.nn.functional.nll_loss(inp, target)
--- tests/operators/test_loss.py
+++ tests/operators/test_loss.py
@@ -11,6 +11,7 @@ from tests.helper import decorator
 from msamp.common.dtype import Dtypes
 from msamp.nn import LinearReplacer
 from msamp.common.tensor import ScalingMeta
+from msamp.operators.activation import Activation
 from msamp.operators.loss_fn import Loss_fn
 
 
@@ -55,3 +56,44 @@ class LossfnTestClass(unittest.TestCase):
         self.assertTrue(torch.allclose(model1.weight.grad.float(), model2.weight.grad.float(), atol=1e-3))
         
         # python -m unittest tests.operators.test_loss.LossfnTestClass.test_scalingSum
+        
+    @decorator.cuda_test
+    def test_scalingNLLloss(self):
+        '''Test the function Loss_fn.nll_loss().'''  
+        # input = torch.randn((3, 4), dtype=torch.float16, device='cuda', requires_grad=True)   #! here cannot set requires_grad=True, since FP8GemmFunction has not yet support grad computing for the very first input tensor
+        input = torch.randn((3, 4), dtype=torch.float16, device='cuda')
+        target = torch.randint(0, 4, (3,), dtype=torch.long, device='cuda')  
+  
+        linear = torch.nn.Linear(4, 8, bias=False).cuda().half()  
+        
+        model1 = LinearReplacer.replace(linear, Dtypes.kfloat16)  
+        inner1 = model1(input)  
+        out1 = Activation.log_softmax(inner1, dim=1)
+        loss1 = Loss_fn.nll_loss(out1, target)  
+        self.assertTrue(loss1.requires_grad)  
+          
+        model2 = LinearReplacer.replace(linear, Dtypes.kfloat16, enabling_fp8_activation=True)  
+        inner2 = model2(input)  
+        out2 = Activation.log_softmax(inner2, dim=1)
+        self.assertTrue(out2.is_fp8_form == True)  
+        loss2 = Loss_fn.nll_loss(out2, target)  
+        self.assertTrue(loss2.requires_grad)  
+          
+        # print(f"loss1: {loss1}, grad_fn: {loss1.grad_fn}")
+        # print(f"loss2: {loss2}, grad_fn: {loss2.grad_fn}")
+        self.assertAlmostEqual(loss1.item(), loss2.item(), delta=5e-2)  
+          
+        # Backward pass  
+        loss1.backward()  
+        loss2.backward()  
+          
+        # Check that the gradients are very close  
+        # print(f"model1.weight.grad: {model1.weight.grad}")
+        # print(f"model2.weight.grad: {model2.weight.grad}")
+        self.assertTrue(torch.allclose(model1.weight.grad.float(), model2.weight.grad.float(), atol=5e-2))  
+          
+        # Clean up for next test  
+        model1.weight.grad = None  
+        model2.weight.grad = None 
+        
+        # python -m unittest tests.operators.test_loss.LossfnTestClass.test_scalingNLLloss
