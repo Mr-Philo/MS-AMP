8741cbbf0c32c0361a605996b783400265f27fdd
Mr-Philo <1347549342@qq.com>
2025-01-09 03:17:57 +0000

    fix a quant ablation param from clip 0.99 to clip 0.999: fix format from e1m2 to e2m1

--- msamp/megatron/layers.py
+++ msamp/megatron/layers.py
@@ -78,9 +78,12 @@ class FP8LinearWithGradAccumulationAndAsyncCommunication(torch.autograd.Function
         ], f"Unsupported fp4_quantize_scheme: {fp4_quantize_scheme}"
         
         a_fp4_args = {
+            # "e1m2_clip_0.97_compensation": {"format": "e1m2", "token_wise": True, "outlier_clip": True, "clip_threshold": 0.97, "nan_existed": False, "residual_compensation": True},
             # "e1m2_clip_0.97_compensation": {"format": "e1m2", "token_wise": False, "outlier_clip": True, "clip_threshold": 0.97, "nan_existed": False, "residual_compensation": True},
-            # "e1m2_clip_0.97_compensation": {"format": "e1m2", "token_wise": False, "outlier_clip": False, "nan_existed": False},
-            "e1m2_clip_0.97_compensation": {"format": "e1m2", "token_wise": False, "outlier_clip": True, "nan_existed": False, "clip_threshold": 0.999},
+            # "e1m2_clip_0.97_compensation": {"format": "e2m1", "token_wise": True, "outlier_clip": False, "nan_existed": False},
+            # "e1m2_clip_0.97_compensation": {"format": "e2m1", "token_wise": False, "outlier_clip": False, "nan_existed": False},
+            "e1m2_clip_0.97_compensation": {"format": "e2m1", "token_wise": False, "outlier_clip": True, "nan_existed": False, "clip_threshold": 0.999},
+            # -------------------------------------
             "e1m2_clip_0.99_compensation": {"format": "e1m2", "token_wise": True, "outlier_clip": True, "clip_threshold": 0.99, "nan_existed": False, "residual_compensation": True},
             "e1m2_clip_0.97_compensation_gemm": {"format": "e1m2", "token_wise": True, "outlier_clip": True, "clip_threshold": 0.97, "nan_existed": False, "residual_compensation": False, "return_residual": True},
             "e2m1_token_wise":  {"format": "e2m1", "token_wise": True, "outlier_clip": False, "nan_existed": False},
